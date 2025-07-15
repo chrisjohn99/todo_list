9123de41e3fe06ad1867b63105000293f9d799e3
Chris John <cxjohn@pm.me>
2025-07-14 23:28:25 -0400

    adding updated object with tests

--- todolist.js
+++ todolist.js
@@ -1,34 +1,4 @@
-class Todo {
-  static DONE_MARKER = "X";
-  static UNDONE_MARKER = " ";
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
-
+const Todo = require("./todo")
 class TodoList {
   constructor(title) {
     this.title = title;
@@ -151,3 +121,5 @@ todo5.markDone();
 list.markAllUndone();
 
 console.log(list.toArray());
+
+module.exports = TodoList;
